34a997e1b082faa92f28e86112c150d60c9c8409
shaukathussain <shaukathussain1@hotmail.com>
2023-01-26 23:09:07 +0500

    iterators

--- unit_test.py
+++ unit_test.py
@@ -51,67 +51,72 @@ import unittest
 #         print("This test should only run on Windows")
 
 
-def regional_jet():
-    print('This is a regional jet...')
-    return True
+# def regional_jet():
+#     print('This is a regional jet...')
+#     return True
 
 
-def get_daily_movie():
-    print('Retrieving the movie set to play on today\'s flight...')
-    return 'Parasite'
+# def get_daily_movie():
+#     print('Retrieving the movie set to play on today\'s flight...')
+#     return 'Parasite'
 
 
-def get_licensed_movies():
-    print('Retrieving the list of licenses movies from the database...')
-    licensed_movies = ['Parasite', 'Nomadland', 'Roma']
-    return licensed_movies
+# def get_licensed_movies():
+#     print('Retrieving the list of licenses movies from the database...')
+#     licensed_movies = ['Parasite', 'Nomadland', 'Roma']
+#     return licensed_movies
 
 
-def get_wifi_status():
-    print('Checking WiFi signal...')
-    print('WiFi is active')
-    return True
+# def get_wifi_status():
+#     print('Checking WiFi signal...')
+#     print('WiFi is active')
+#     return True
 
 
-def get_device_temp():
-    print('Reading the temperature of the entertainment system device...')
-    return 33.2
+# def get_device_temp():
+#     print('Reading the temperature of the entertainment system device...')
+#     return 33.2
 
 
-def get_maximum_display_brightness():
-    print('Calculating maximum display brightness in nits...')
-    return 399.99999999
+# def get_maximum_display_brightness():
+#     print('Calculating maximum display brightness in nits...')
+#     return 399.99999999
 
 
-class EntertainmentSystemTests(unittest.TestCase):
+# class EntertainmentSystemTests(unittest.TestCase):
 
-    @unittest.skipIf(regional_jet(), 'Not available on regional jets')
-    def test_movie_license(self):
-        daily_movie = get_daily_movie()
-        licensed_movies = get_licensed_movies()
-        self.assertIn(daily_movie, licensed_movies)
+#     @unittest.skipIf(regional_jet(), 'Not available on regional jets')
+#     def test_movie_license(self):
+#         daily_movie = get_daily_movie()
+#         licensed_movies = get_licensed_movies()
+#         self.assertIn(daily_movie, licensed_movies)
 
-    @unittest.skipUnless(regional_jet() is False, 'Not available on regional jets')
-    def test_wifi_status(self):
-        wifi_enabled = get_wifi_status()
-        self.assertTrue(wifi_enabled)
+#     @unittest.skipUnless(regional_jet() is False, 'Not available on regional jets')
+#     def test_wifi_status(self):
+#         wifi_enabled = get_wifi_status()
+#         self.assertTrue(wifi_enabled)
 
-    def test_device_temperature(self):
-        if regional_jet():
-            self.skipTest('Not available on regional jets')
+#     def test_device_temperature(self):
+#         if regional_jet():
+#             self.skipTest('Not available on regional jets')
 
-        device_temp = get_device_temp()
-        self.assertLess(device_temp, 35)
+#         device_temp = get_device_temp()
+#         self.assertLess(device_temp, 35)
 
-    def test_maximum_display_brightness(self):
-        if regional_jet():
-            self.skipTest('Not available on regional jets')
+#     def test_maximum_display_brightness(self):
+#         if regional_jet():
+#             self.skipTest('Not available on regional jets')
 
-        brightness = get_maximum_display_brightness()
-        self.assertAlmostEqual(brightness, 400)
-
-
-unittest.main()
+#         brightness = get_maximum_display_brightness()
+#         self.assertAlmostEqual(brightness, 400)
 
 
-unittest.main()
+# unittest.main()
+
+
+# unittest.main()
+
+
+########################### Expected Failures ###########################
+
+
